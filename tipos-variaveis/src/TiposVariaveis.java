e47298c9465e4071a6a040827db9ef6fffe425f3
jeppa1 <jadson.jeppa@gmail.com>
2024-05-14 01:18:54 -0300

    exercicio 2

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,4 +2,14 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         System.out.println("Estou apenas começando, xD");
     }
+    public class TiposDados {
+        public static void main (String[] args) {
+            byte idade = 31;
+            short ano = 2024;
+            int cep = 59296686;    //se começar com zero, tem que colocar outro tipo, no caso String
+            long = 10204197414L;   //se começar com zero, tem que colocar outro tipo, no caso String
+            float pi = 3.14F;
+            double  salario = 1500;
+        }
+    }
 }
