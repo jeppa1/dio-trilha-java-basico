6c9aba3db3e308a05b756af8a2fc6f742d9310e9
jeppa1 <jadson.jeppa@gmail.com>
2024-05-17 02:14:47 -0300

    ajuste de segurança

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -6,8 +6,8 @@ public class TiposVariaveis {
         public static void main (String[] args) {
             byte idade = 31;
             short ano = 2024;
-            int cep = 59296686;    //se começar com zero, tem que colocar outro tipo, no caso String
-            long = 10204197414L;   //se começar com zero, tem que colocar outro tipo, no caso String
+            int cep = 55555555;    //se começar com zero, tem que colocar outro tipo, no caso String
+            long = 98765432109L;   //se começar com zero, tem que colocar outro tipo, no caso String
             float pi = 3.14F;
             double  salario = 1500;
         }
