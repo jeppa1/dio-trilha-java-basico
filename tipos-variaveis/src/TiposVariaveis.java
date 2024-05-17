6b63867f816174e50535308db1047db84d395a14
jeppa1 <jadson.jeppa@gmail.com>
2024-05-17 02:28:12 -0300

    commit 17/05

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,6 +1,6 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Estou apenas começando, xD");
+        System.out.println("Bem vindo, puxe a cadeira, pegue um cafezinho e acenda um.");
     }
     public class TiposDados {
         public static void main (String[] args) {
@@ -10,6 +10,10 @@ public class TiposVariaveis {
             long = 98765432109L;   //se começar com zero, tem que colocar outro tipo, no caso String
             float pi = 3.14F;
             double  salario = 1500;
+
+            short numeroCurto = 1;
+            int numeroNormal = numeroCurto;
+            short numeroCurto2 = (short) numeroNormal;   //casting
         }
     }
 }
